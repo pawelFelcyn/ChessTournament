615b5099bde30c7bc2bb18171c6a3dee0eb8ebaa
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-06-25 14:27:40 +0200

    Add User and Tournament entities

---
+++ Domain/Entities/Tournament.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Tournament
+    {
+        public int Id { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string Street { get; set; }
+        public string BuildingNumber { get; set; }
+        public string LocalNumber { get; set; }
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+        public int? MaxPlayers { get; set; }
+        public decimal? CostPerPlayer { get; set; }
+        public string Description { get; set; }
+        public TournamentStatus Status { get; set; }
+        public int NumberOfRounds { get; set; }
+
+        public virtual List<User> Players { get; set; }
+    }
+}
---
+++ Domain/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string RoleName { get; set; }
+        public string Club { get; set; }
+        public string City { get; set; }
+        public DateTime? Birthdate { get; set; }
+        public DateTime DateOfAppending { get; set; }
+        public string PasswordHash { get; set; }
+
+        public virtual List<Tournament> Tournaments { get; set; }
+    }
+}
