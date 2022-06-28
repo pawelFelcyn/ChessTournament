bf03bb1054ec108124f235d8df2780ebd61efdfd
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-06-28 18:01:21 +0200

    Add account dtos

---
+++ Application/Dtos/RegisterDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos
+{
+    public record RegisterDto(string FirstName, string LastName, string Email, string RoleName, string Club, string City, DateTime? Birthdate, string Password, string ConfirmPassword);
+}
