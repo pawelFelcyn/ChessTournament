206b2d27474a818f0a46197dac35d9cf3c04c40d
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-06-28 18:02:26 +0200

    RegisterDto validation

---
+++ Infrastructure/Helpers/EmailValidationHelper.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Interfaces;
+using Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Helpers
+{
+    public class EmailValidationHelper : IEmailValidationHelper
+    {
+        private readonly ChessTournamentDbContext _dbContext;
+
+        public EmailValidationHelper(ChessTournamentDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public bool IsTaken(string value)
+        {
+            return _dbContext.Users.Any(u => u.Email == value);
+        }
+    }
+}
