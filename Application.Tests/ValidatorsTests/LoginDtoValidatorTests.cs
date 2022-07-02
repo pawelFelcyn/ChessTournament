d0b1ef3e0f922d864ae773bc226ed538bdcfc30d
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-07-02 19:24:53 +0200

    LoginDtoValidator + Tests

---
+++ Application.Tests/ValidatorsTests/LoginDtoValidatorTests.cs
@@ -0,0 +1,56 @@
+﻿using Application.Dtos;
+using Application.Validation;
+using FluentValidation.TestHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Application.Tests.ValidatorsTests
+{
+    public class LoginDtoValidatorTests : IClassFixture<LoginDtoValidator>
+    {
+        private readonly LoginDtoValidator _validator;
+
+        public LoginDtoValidatorTests(LoginDtoValidator validator)
+        {
+            _validator = validator;
+        }
+
+        [Fact]
+        public void Validate_ForValidModel_ReturnsProperValidationResult()
+        {
+            var model = new LoginDto("email", "password");
+
+            var result = _validator.TestValidate(model);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void Validate_ForInvalidEmail_ReturnsProperValidationResult(string email)
+        {
+            var model = new LoginDto(email, "password");
+
+            var result = _validator.TestValidate(model);
+
+            result.ShouldHaveValidationErrorFor(l => l.Email);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void Validate_ForInvalidPassword_ReturnsProperValidationResult(string password)
+        {
+            var model = new LoginDto("email", password);
+
+            var result = _validator.TestValidate(model);
+
+            result.ShouldHaveValidationErrorFor(l => l.Password);
+        }
+    }
+}
---
+++ Application/Validation/LoginDtoValidator.cs
@@ -0,0 +1,33 @@
+﻿using Application.Dtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Validation
+{
+    public class LoginDtoValidator : AbstractValidator<LoginDto>
+    {
+        public LoginDtoValidator()
+        {
+            RuleForEmail();
+            RuleForPassword();
+        }
+
+        private void RuleForEmail()
+        {
+            RuleFor(l => l.Email)
+                .NotEmpty()
+                .WithMessage("Email must not be empty");
+        }
+
+        private void RuleForPassword()
+        {
+            RuleFor(l => l.Password)
+                .NotEmpty()
+                .WithMessage("Password must not be empty");
+        }
+    }
+}
